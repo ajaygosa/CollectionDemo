6d043ffe2b7552f0036767304879fa69d69f2ce7
Ajay Gosavi <ajaygosavi001@gmail.com>
2022-11-15 13:52:24 +0530

    code for LL

---
+++ CollectionDemo/src/LinkedList.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class LinkedList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ArrayList al = new ArrayList();
+		al.add("jagruti");
+		al.add("Anil");
+		
+		for(Object str :al)
+		{
+			System.out.println(str);
+		}
+	}
+
+}
